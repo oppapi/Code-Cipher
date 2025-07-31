ddcf1354da98e85a60d01f5bef3a445cea5a2e75
John Carl Fetalver Ola <johncarl4102@gmail.com>
2025-07-31 23:52:29 +0800

    include ascii value of char

--- Code Cipher/Form1.cs
+++ Code Cipher/Form1.cs
@@ -12,6 +12,8 @@ namespace Code_Cipher
 {
     public partial class Form1 : Form
     {
+        private char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
         public Form1()
         {
             InitializeComponent();
@@ -30,6 +32,26 @@ namespace Code_Cipher
         private void btnEncrypt_Click(object sender, EventArgs e)
         {
             btnEncrypt.Text = (btnEncrypt.Text=="Encrypt") ? "Decrypt" : "Encrypt";
+            foreach (char c in txtPlain.Text.ToUpper())
+            {
+                if (convertChar(c) != -1)
+                {
+                    txtCipher.Text += alphabet[(index + 3) % 26];
+                }
+                else
+                {
+                    txtCipher.Text += c;
+                }
+            }
+        }
+
+        private int convertChar(char c)
+        {
+            if (char.IsLetter(c))
+            {
+                return char.ToLower(c) - 'a' + 1;
+            }
+            return -1;
         }
     }
 }
